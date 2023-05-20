2636d444f7d327d72092bd229a44577fbe54a02e
excell-09 <excell.agcb12@gmail.com>
2023-05-20 11:43:41 +0700

    create testing production

--- portal_berita/settings.py
+++ portal_berita/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t=tt2d99r3#-q(1&@)z8yb7pc6)0jppr6etc5mui_g4l)pn^l-'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+if os.getenv("PRODUCTION") == "true" :
+    DEBUG = False
+
+
 ALLOWED_HOSTS = []
+
+if not DEBUG :
+    ALLOWED_HOSTS = [".vercel.app"]
+
+
 
 
 # Application definition
--- portal_berita/wsgi.py
+++ portal_berita/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from .settings import DEBUG
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal_berita.settings')
 
 application = get_wsgi_application()
+
+if not DEBUG :
+    app = application 
+
