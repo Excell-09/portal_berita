3e3b5b69601d73e0aecef70ebd00d24453997b0e
excell-09 <excell.agcb12@gmail.com>
2023-05-29 08:58:45 +0700

    fix setting.py

--- portal_berita/settings.py
+++ portal_berita/settings.py
@@ -164,7 +164,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    os.getenv("CLIENT_URL")
 ]
 
 
